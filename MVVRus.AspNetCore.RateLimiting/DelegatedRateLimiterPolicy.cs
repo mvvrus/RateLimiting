e0c2239750232144bf03f70dcfff5cf4ff113760
mvvrus <vmurav@yandex.ru>
2025-06-04 17:09:44 +0300

    Add project files.

---
+++ MVVRus.AspNetCore.RateLimiting/DelegatedRateLimiterPolicy.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace MVVRus.AspNetCore.RateLimiting
+{
+    public class DelegatedRateLimiterPolicy<TPartitionKey> : IRateLimiterPolicy<TPartitionKey>
+    {
+        Func<HttpContext, RateLimitPartition<TPartitionKey>> _partitioner;
+        public Func<OnRejectedContext, CancellationToken, ValueTask>? OnRejected { get; init; }
+
+        public DelegatedRateLimiterPolicy(Func<HttpContext, RateLimitPartition<TPartitionKey>> partitioner, 
+            Func<OnRejectedContext, CancellationToken, ValueTask>? onRejected)
+        {
+            _partitioner = partitioner;
+            OnRejected=onRejected;
+        }
+
+        public RateLimitPartition<TPartitionKey> GetPartition(HttpContext httpContext)
+        {
+            return _partitioner(httpContext);
+        }
+    }
+}
