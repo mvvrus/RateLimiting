6884f215581c8dafd23cabdf59cb9855f1d7e02e
mvvrus <vmurav@yandex.ru>
2025-07-21 05:44:19 +0300

    Make namespace ManageLifetimeLimiter for MVVRus.Extensions.RateLimiting project.

---
+++ MVVRus.Extensions.RateLimiting/ManagedLifetimeLimiter.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.RateLimiting;
+
+namespace MVVRus.Extensions.RateLimiting
+{
+    public class ManagedLifetimeLimiter : RateLimiter
+    {
+        RateLimiter? _inner;
+
+        public ManagedLifetimeLimiter(RateLimiter Inner)
+        {
+            _inner=Inner;
+        }
+
+        public override TimeSpan? IdleDuration => _inner is null?TimeSpan.MaxValue:null;
+
+        public override RateLimiterStatistics? GetStatistics()
+        {
+            return _inner?.GetStatistics()??throw new ObjectDisposedException(nameof(_inner));
+        }
+
+        protected override ValueTask<RateLimitLease> AcquireAsyncCore(Int32 permitCount, CancellationToken cancellationToken)
+        {
+            return _inner?.AcquireAsync(permitCount, cancellationToken)??throw new ObjectDisposedException(nameof(_inner));
+        }
+
+        protected override RateLimitLease AttemptAcquireCore(Int32 permitCount)
+        {
+            return _inner?.AttemptAcquire(permitCount) ?? throw new ObjectDisposedException(nameof(_inner));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing) {
+                RateLimiter? inner = Interlocked.Exchange(ref _inner, null);
+                if(inner!=null) inner.Dispose();
+            }       
+        }
+
+        protected override async ValueTask DisposeAsyncCore()
+        {
+            RateLimiter? inner = Interlocked.Exchange(ref _inner, null);
+            if(inner!=null) await inner.DisposeAsync();
+            await base.DisposeAsyncCore();
+        }
+
+    }
+}
---
+++ MVVRus.Extensions.RateLimiting/ManagedLifetimePartition.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.RateLimiting;
+
+namespace MVVRus.Extensions.RateLimiting
+{
+    public static  class ManagedLifetimePartition
+    {
+        public static Func<TKey, RateLimitPartition<TKey>> GetManagedLifetimeLimiter<TKey>(
+            Func<TKey, RateLimiter> inner,
+            Action<ManagedLifetimeLimiter, TKey, Object?>? registrar = null,
+            Object? state = null)
+        {
+            return (TKey key) => {
+                ManagedLifetimeLimiter limiter = new ManagedLifetimeLimiter(inner(key));
+                registrar?.Invoke(limiter, key, state);
+                return new RateLimitPartition<TKey>(key, _ => limiter);
+            };
+        }
+
+        public static Func<TKey, RateLimitPartition<TKey>> GetManagedLifetimeLimiter<TKey>(
+            Func<TKey, RateLimitPartition<TKey>> inner,
+            Action<ManagedLifetimeLimiter, TKey, Object?>? registrar = null,
+            Object? state = null)
+        {
+            return GetManagedLifetimeLimiter(key=>inner(key).Factory(key), registrar, state);
+        }
+
+    }
+}
