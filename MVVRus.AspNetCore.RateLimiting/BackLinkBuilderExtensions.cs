4a863a4630c8943a6f568c515bf094ffecef1bda
mvvrus <vmurav@yandex.ru>
2025-09-17 06:42:08 +0300

    Add a middleware for the backlink feature

---
+++ MVVRus.AspNetCore.RateLimiting/BackLinkBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVRus.AspNetCore.RateLimiting
+{
+    public static class BackLinkBuilderExtensions
+    {
+        public static IApplicationBuilder UseBackLink(this IApplicationBuilder app) 
+        {
+            return app.UseMiddleware<BackLinkMiddleware>();
+        }
+    }
+}
---
+++ MVVRus.AspNetCore.RateLimiting/BackLinkMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace MVVRus.AspNetCore.RateLimiting
+{
+    internal class BackLinkMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BackLinkMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            IHttpContextBackLinkFeature back_link = new BackLinkImpl(context);
+            try {
+                context.Features[typeof(IHttpContextBackLinkFeature)] = back_link;
+                await _next(context);
+            }
+            finally {
+                context.Features[typeof(IHttpContextBackLinkFeature)] = null;
+                back_link.Dispose();
+            }
+        }
+    }
+}
