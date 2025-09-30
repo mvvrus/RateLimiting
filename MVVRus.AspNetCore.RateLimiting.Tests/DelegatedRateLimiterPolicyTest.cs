7ce054487822dd9b9ca09aeda79858fa128be132
mvvrus <vmurav@yandex.ru>
2025-09-30 05:51:50 +0300

    Continue with test via TestHost - make RateLimiter tests via SendAsync, not HttpClient.

--- MVVRus.AspNetCore.RateLimiting.Tests/DelegatedRateLimiterPolicyTest.cs
+++ MVVRus.AspNetCore.RateLimiting.Tests/DelegatedRateLimiterPolicyTest.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading.RateLimiting;
 
 namespace MVVRus.AspNetCore.RateLimiting.Tests
 {
@@ -9,14 +14,50 @@ namespace MVVRus.AspNetCore.RateLimiting.Tests
         [Fact]
         public async Task Test1()
         {
-            IHostBuilder host_builder = new HostBuilder()
-                .ConfigureWebHost(webHost => {
-                    webHost.Configure(_ => { });
-                    webHost.UseTestServer(); 
-                } );
-            await host_builder.StartAsync();
-            //IHost host = host_builder.Build();
-            //await host.StartAsync();
+            ThisTestAppConfig tac = new ThisTestAppConfig();
+            IHostBuilder host_builder = new HostBuilder().ConfigureWebHost(tac.ConfigureTestApp);
+            IHost host = await host_builder.StartAsync();
+            TestServer server = host.GetTestServer();
+            server.BaseAddress = new Uri("http://localhost/");
+            HttpContext context;
+            for(int i = 0; i < 6; i++) {
+                context = await server.SendAsync(ctx=>ctx.Request.Path="/ping", default);
+                Assert.InRange(context.Response.StatusCode, 200, 299);
+            }
+        }
+
+
+
+        class ThisTestAppConfig : TestAppConfig
+        {
+            protected override void Configure(IApplicationBuilder app)
+            {
+                app.UseRateLimiter();
+                base.Configure(app);
+            }
+
+            protected override void ConfigureRest(IApplicationBuilder app)
+            {
+                app.Run(async ctx => { await Task.Yield(); ctx.Response.StatusCode=StatusCodes.Status204NoContent; });
+                base.ConfigureRest(app);
+            }
+
+            protected override void ConfigureRouting(IEndpointRouteBuilder builder)
+            {
+                base.ConfigureRouting(builder);
+            }
+
+            protected override void ConfigureServices(IServiceCollection services)
+            {
+                services.AddRateLimiter(options => {
+                    options.GlobalLimiter = PartitionedRateLimiter.Create(
+                        (HttpContext ctx)=>RateLimitPartition.GetFixedWindowLimiter(
+                            0, _ => new FixedWindowRateLimiterOptions() { PermitLimit=5, Window=TimeSpan.FromSeconds(10)}
+                        )
+                    );
+                });
+                base.ConfigureServices(services);
+            }
         }
     }
 }
---
+++ MVVRus.AspNetCore.RateLimiting.Tests/TestAppConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MVVRus.AspNetCore.RateLimiting.Tests
+{
+    internal class TestAppConfig
+    {
+        public TestAppConfig() { }
+
+        public void ConfigureTestApp(IWebHostBuilder webHost)
+        {
+            webHost.ConfigureServices(ConfigureAppServices);
+            webHost.Configure(ConfigureAppPipeline);
+            webHost.UseTestServer();
+        }
+
+        private void ConfigureAppServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            ConfigureServices(services);
+        }
+
+        private void ConfigureAppPipeline(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            Configure(app);
+            app.UseEndpoints(ConfigureRouting);
+            ConfigureRest(app);
+        }
+
+        protected virtual void ConfigureServices(IServiceCollection services) { }
+        protected virtual void ConfigureRouting(IEndpointRouteBuilder builder) { }
+        protected virtual void Configure(IApplicationBuilder app) { }
+        protected virtual void ConfigureRest(IApplicationBuilder app) { }
+    }
+
+}
