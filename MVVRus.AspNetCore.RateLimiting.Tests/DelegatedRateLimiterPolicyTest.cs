3030bd3955f3ff4a9c44aedb1330ca7416eae36c
mvvrus <vmurav@yandex.ru>
2025-10-05 06:10:26 +0300

    Change rate limiter test to use concurent requests.

--- MVVRus.AspNetCore.RateLimiting.Tests/DelegatedRateLimiterPolicyTest.cs
+++ MVVRus.AspNetCore.RateLimiting.Tests/DelegatedRateLimiterPolicyTest.cs
@@ -6,39 +6,66 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.RateLimiting;
+using MVVRus.Threading.AsyncSynchroPrimitives;
 
 namespace MVVRus.AspNetCore.RateLimiting.Tests
 {
     public class DelegatedRateLimiterPolicyTest
     {
-        [Fact]
+        [Fact(Timeout = 0)]
         public async Task Test1()
         {
-            ThisTestAppConfig tac = new ThisTestAppConfig();
+            int i;
+            Int32 num_requests = 6, limit = 5;
+            ThisTestAppConfig tac = new ThisTestAppConfig(num_requests);
             IHostBuilder host_builder = new HostBuilder().ConfigureWebHost(tac.ConfigureTestApp);
             IHost host = await host_builder.StartAsync();
             TestServer server = host.GetTestServer();
             server.BaseAddress = new Uri("http://localhost/");
             HttpContext context;
-            for(int i = 0; i < 6; i++) {
-                context = await server.SendAsync(ctx=>ctx.Request.Path="/ping", default);
-                Assert.InRange(context.Response.StatusCode, 200, 299);
+            Task<HttpContext>[] sendResults = new Task<HttpContext>[num_requests];
+            for(i = 0; i < num_requests; i++) {
+                sendResults[i] = server.SendAsync(ctx=>ctx.Request.Path="/ping", default);
             }
+            await tac.QuorumEvent.WaitAsync();
+            tac.Continue();
+
+            for(i=0; i<num_requests; i++) {
+                context = await sendResults[i];
+                if (i<limit) Assert.InRange(context.Response.StatusCode, 200, 299);
+                else Assert.NotInRange(context.Response.StatusCode, 200, 299);
+            }
+
         }
 
 
 
         class ThisTestAppConfig : TestAppConfig
         {
+            AsyncManualResetEvent _mre;
+            public AsyncCountdownEvent QuorumEvent { get; }
+
+            public ThisTestAppConfig(Int32 quorumCount)
+            {
+                _mre=new AsyncManualResetEvent();
+                QuorumEvent = new AsyncCountdownEvent(quorumCount);
+            }
+
+            public void Continue()
+            {
+                _mre.Set();
+            }
+
             protected override void Configure(IApplicationBuilder app)
             {
+                app.Use(next => async context=>{ QuorumEvent.Signal(); await next(context); });
                 app.UseRateLimiter();
                 base.Configure(app);
             }
 
             protected override void ConfigureRest(IApplicationBuilder app)
             {
-                app.Run(async ctx => { await Task.Yield(); ctx.Response.StatusCode=StatusCodes.Status204NoContent; });
+                app.Run(async ctx => { await _mre.WaitAsync(); ctx.Response.StatusCode=StatusCodes.Status204NoContent; });
                 base.ConfigureRest(app);
             }
 
@@ -51,8 +78,8 @@ namespace MVVRus.AspNetCore.RateLimiting.Tests
             {
                 services.AddRateLimiter(options => {
                     options.GlobalLimiter = PartitionedRateLimiter.Create(
-                        (HttpContext ctx)=>RateLimitPartition.GetFixedWindowLimiter(
-                            0, _ => new FixedWindowRateLimiterOptions() { PermitLimit=5, Window=TimeSpan.FromSeconds(10)}
+                        (HttpContext ctx)=>RateLimitPartition.GetConcurrencyLimiter(
+                            0, _ => new ConcurrencyLimiterOptions() { PermitLimit=5}
                         )
                     );
                 });
