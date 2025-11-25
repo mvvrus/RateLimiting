78c7e7e6d21b6f7c0fa65aa66e37ab3f9402ab1a
mvvrus <vmurav@yandex.ru>
2025-11-25 03:08:22 +0300

    Add an extension method AddActiveSessionPerGroupLimiter for IServiceCollection.
    Rname the extension method UseActiveSessionLimiting to UseActiveSessionPerGroupLimiting.

--- MVVrus.AspNetCore.ActiveSession.RateLimiting/ActiveSessionLimitingExtensions.cs
+++ MVVrus.AspNetCore.ActiveSession.RateLimiting/ActiveSessionLimitingExtensions.cs
@@ -1,11 +1,29 @@
-﻿namespace MVVrus.AspNetCore.ActiveSession.RateLimiting
+﻿using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace MVVrus.AspNetCore.ActiveSession.RateLimiting
 {
     public static class ActiveSessionLimitingExtensions
     {
-        public static IApplicationBuilder UseActiveSessionLimiting(this IApplicationBuilder app)
+        public static IApplicationBuilder UseActiveSessionPerGroupLimiting(this IApplicationBuilder app)
         {
             app.UseMiddleware<ActiveSessionLimitingMiddleware>();
             return app;
         }
+
+        public static IServiceCollection AddActiveSessionPerGroupLimiting(this IServiceCollection services, ConcurrencyLimiterOptions limiterOptions)
+        {
+            services.Configure<RateLimiterOptions>(AddActiveSessionPerGroupLimiter);
+            return services;
+
+            void AddActiveSessionPerGroupLimiter(RateLimiterOptions options)
+            {
+                ActiveSessionsPerGroupLimiter limiter = new ActiveSessionsPerGroupLimiter(limiterOptions);
+                options.GlobalLimiter = options.GlobalLimiter==null ? 
+                    limiter : PartitionedRateLimiter.CreateChained(options.GlobalLimiter, limiter);
+            }
+
+        }
+
     }
 }
